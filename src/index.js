01e6ac46daad48d5773a84f3ba2cfd5d2291aee0
Andreas Leonard-Calcano <andreasleonard-calcano@Andreass-MacBook-Pro-2.local>
2023-02-03 07:59:34 -0500

    module 3

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
+const movies = require('../src/data/movies.json')
+const {fetchWithTimeout} = require('../src/services');
+
 export function fetchMovies ()  {
-    
+    const resolveFunction = () => movies;
+    fetchWithTimeout(1000).then(() => resolveFunction )
 } 
+
+const moviePromise = fetchMovies();
+
+moviePromise().then((result) => console.log(result));
